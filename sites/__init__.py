8c7be7c9450cdf192658d306b98371c78c91f6f5
nhanb <thanhnhan483@gmail.com>
2014-01-09 19:48:13 +0700

    remove index.yaml from git, add basic kissmanga site implementation

---
+++ sites/__init__.py
@@ -0,0 +1,21 @@
+from url import urlparse
+from google.appengine.api import urlfetch
+
+
+# Factory function, return instance of suitable "site" class from url
+def get_site(url):
+    parsed = urlparse.urlparse(url)
+    if parsed.netloc == 'kissmanga.com':
+        import kissmanga
+        return kissmanga.get_site(url)
+    else:
+        return None
+
+
+# Return whole html string of the fetched url, return None if failed
+def get_html(url):
+    resp = urlfetch.fetch(url)
+    if resp.status_code == 200:
+        return resp.content
+    else:
+        return None
---
+++ sites/kissmanga.py
@@ -0,0 +1,21 @@
+from url import urlparse
+from sites import get_html
+import re
+
+
+class Kissmanga(object):
+
+    def get_chapter_name(self, url):
+        parsed = urlparse.urlparse(url)
+        return parsed.path.split('/')[-1]
+
+    def get_pages(self, url):
+        html = get_html(url)
+
+        # Create regex to match page link
+        pat = re.compile('lstImages\.push\("(.+?)"\);')
+        page_links = pat.findall(html)
+
+        # Regex to match page image file name
+        ipat = re.compile('/([0-9]{3}\.(png|jpg))\?')
+        self.pages = ((ipat.findall(x)[0][0], x) for x in page_links)
