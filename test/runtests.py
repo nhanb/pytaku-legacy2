91d4ba90122137f9f96d2385a27e2fffc4252c61
nhanb <thanhnhan483@gmail.com>
2014-01-10 13:29:45 +0700

    screw that

--- test/runtests.py
+++ test/runtests.py
@@ -5,21 +5,20 @@ import unittest
 
 
 def main():
-    print sys.path
-    ## Add SDK path to, well, PATH
-    #sdk_path = os.environ.get(
-        #'GAE_PATH',  # read environment variable if available
-        #os.environ.get('HOME') + '/google_appengine'  # gae path on travis
-    #)
-    #sys.path.insert(0, sdk_path)
     import dev_appserver
     dev_appserver.fix_sys_path()
 
     # Get test path
     test_path = os.path.dirname(__file__)
 
+    print test_path
+
     suite = unittest.loader.TestLoader().discover(test_path)
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    result = unittest.TextTestRunner(verbosity=2).run(suite)
+    if (result.wasSuccessful()):
+        sys.exit(0)
+    else:
+        sys.exit(1)
 
 if __name__ == '__main__':
     main()
