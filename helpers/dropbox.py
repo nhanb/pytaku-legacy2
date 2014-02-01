30640b5ad87d37d70ce5fa36cdb7bb7e1ee851eb
nhanb <thanhnhan483@gmail.com>
2014-02-02 02:00:40 +0700

    dropbox.upload() via oauth2. Suck on that, oauth1!

--- helpers/dropbox.py
+++ helpers/dropbox.py
@@ -1,4 +1,6 @@
 import ConfigParser
+import urllib
+from google.appengine.api import urlfetch
 
 config = ConfigParser.ConfigParser()
 config.read("config.ini")
@@ -7,3 +9,15 @@ consumer_key = config.get("Dropbox", "ConsumerKey")
 consumer_secret = config.get("Dropbox", "ConsumerSecret")
 token_url = config.get("Dropbox", "TokenURL")
 authorize_url = config.get("Dropbox", "AuthorizeURL")
+files_put_url = 'https://api-content.dropbox.com/1/files_put/sandbox'
+
+
+def upload(name, content, token):
+    params = {
+        'locale': 'en-US',
+        'overwrite': 'true',
+    }
+    url = files_put_url + '/' + name + '?' + urllib.urlencode(params)
+    headers = {'Authorization': 'Bearer ' + token}
+    resp = urlfetch.fetch(url, content, 'PUT', headers)
+    return resp.content
